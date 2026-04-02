f66dc81ddb07134907ac39662f1a32bfc040e455
Nikita-singh-14 <nk2371174@gmail.com>
2026-04-02 21:14:29 +0530

    fix api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+//const API = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://social-app-backend-p1pq.onrender.com',
+  
+  
 });
 
 // Add a request interceptor to include the auth token
